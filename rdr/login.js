e0f02048fcbd35f369008743b9b23019db6680d4
Rohan Scott <rohanlance@gmail.com>
2023-06-05 06:30:46 +1000

    Update login.js

--- rdr/login.js
+++ rdr/login.js
@@ -1,13 +1,13 @@
 
 	var firebaseConfig = {
-		apiKey: "<?php echo $firebaseConfig['apiKey']; ?>",
-		authDomain: "<?php echo $firebaseConfig['authDomain']; ?>",
-		databaseURL: "<?php echo $firebaseConfig['databaseURL']; ?>",
-		projectId: "<?php echo $firebaseConfig['projectId']; ?>",
-		storageBucket: "<?php echo $firebaseConfig['storageBucket']; ?>",
-		messagingSenderId: "<?php echo $firebaseConfig['messagingSenderId']; ?>",
-		appId: "<?php echo $firebaseConfig['appId']; ?>"
-	};
+		apiKey: "AIzaSyDOZA0ojbWAaeWwx0gL7kenlNm94Fo38BY",
+		authDomain: "korean-reader.firebaseapp.com",
+		databaseURL: "https://korean-reader.firebaseio.com",
+		projectId: "korean-reader",
+		storageBucket: "korean-reader.appspot.com",
+		messagingSenderId: "410562108352",
+		appId: "1:410562108352:web:f42d6c8b329d8e54460625"
+	 };
 
 	firebase.initializeApp(firebaseConfig);
 	initialize();
