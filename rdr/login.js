e4fdc03752e816417c3e113c423da6e60e37fec2
Rohan Scott <rohanlance@gmail.com>
2023-06-12 19:28:40 +1000

    s

--- rdr/login.js
+++ rdr/login.js
@@ -105,23 +105,28 @@ window.handleCredentialResponse = (response) => {
       }
       // Create user with email and pass.
       // [START createwithemail]
-      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode == 'auth/weak-password') {
-          alert('The password is too weak.');
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-		return;
-        // [END_EXCLUDE]
-      });
-	document.querySelector('.login-container').style.display = 'block';
-	document.querySelector('.signup-container').style.display = 'none';
-	sendEmailVerification();
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // The user has been created successfully, now you can switch the view
+      // and send the verification email
+      document.querySelector('.login-container').style.display = 'block';
+      document.querySelector('.signup-container').style.display = 'none';
+      sendEmailVerification();
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      return;
+      // [END_EXCLUDE]
+    });
       // [END createwithemail]
     }
 
