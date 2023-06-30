27c06a4175facaab92802f4c9b87710a97404f23
Rohan Scott <rohanlance@gmail.com>
2023-06-30 19:58:52 +1000

    Update read.js

--- rdr/read.js
+++ rdr/read.js
@@ -1105,7 +1105,7 @@ docRef.get()
                     words: firebase.firestore.FieldValue.arrayUnion(...wordsByType[type])
                 })
                 //.then(() => console.log(`Vocabulary of type ${type} updated successfully in Fire DB!`))
-				.then(() => printFireDBVocabItems();
+				.then(() => printFireDBVocabItems())
                 .catch((error) => console.error(`Error updating vocabulary of type ${type} in Fire DB:`, error));
             }
         });
