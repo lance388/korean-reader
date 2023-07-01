a0da89edc8a5c31e31839ca5c391644d6656b412
Rohan Scott <rohanlance@gmail.com>
2023-07-01 13:11:56 +1000

    Update read.js

--- rdr/read.js
+++ rdr/read.js
@@ -173,7 +173,7 @@ async function onAuthStateChanged(user) {
         //logUser(user);
         displaySigninElements("signedInMode");
         signedInState="signedIn";
-		
+		//
         // Wait for the migration to finish
         await checkAndMigrateData(user.uid);
     } else {
